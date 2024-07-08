3d2a6a22a6474b5250cb2baf4996631769cc0f1f
smilepayz <163798308+smilepayz@users.noreply.github.com>
2024-07-08 11:05:21 +0800

    Update pay_in_request_demo.js

--- v2/pay_in_request_demo.js
+++ v2/pay_in_request_demo.js
@@ -102,8 +102,8 @@ const sandboxMerchanteCode = "6a58a603e5043290f4097ee4a7745661b3656932d4eebc3106
 const merchantSecret = "95b57c46b8c2e068982be23fb669a80612cad68e6ce6ba4f5af9ec20d23bb274";
 const merchantSecretSandbox = "sandbox-20019";
 const merchanteId = "20019";
-const sandboxBaseDomain = "sandbox-gateway-test.smilepayz.com";
-const baseDomain = "gateway-test.smilepayz.com";
+const sandboxBaseDomain = "sandbox-gateway.smilepayz.com";
+const baseDomain = "gateway.smilepayz.com";
 payInRequest(merchanteId,merchantSecret,baseDomain);
 
 //********** end post ***************
